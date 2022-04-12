8cbe02ddacb1a44effa3e60d02168d65dfd819ce
abdusalomov0102 <abdusalomov0425@gmail.com>
2022-04-12 16:52:21 +0500

    first commit

---
+++ src/main/java/blog/project/SpringbootBlogProjectApplication.java
@@ -0,0 +1,13 @@
+package blog.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBlogProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBlogProjectApplication.class, args);
+	}
+
+}
